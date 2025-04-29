6ff31d2b46af64a309ebfed8d36453b899bce546
zohaibhassan9 <153729000+zohaibhassan9@users.noreply.github.com>
2025-04-29 15:22:17 +0500

    Lot of work done

--- loader.js
+++ loader.js
@@ -1,14 +1,14 @@
-function loadComponent(selector, filePath) {
-    fetch(filePath)
-      .then(response => response.text())
-      .then(html => {
-        document.querySelector(selector).innerHTML = html;
-      })
-      .catch(error => console.error(`Error loading ${filePath}:`, error));
-  }
+// function loadComponent(selector, filePath) {
+//     fetch(filePath)
+//       .then(response => response.text())
+//       .then(html => {
+//         document.querySelector(selector).innerHTML = html;
+//       })
+//       .catch(error => console.error(`Error loading ${filePath}:`, error));
+//   }
   
-  window.addEventListener('DOMContentLoaded', () => {
-    loadComponent('#header-placeholder', 'header.html');
+//   window.addEventListener('DOMContentLoaded', () => {
+//     loadComponent('#header-placeholder', 'header.html');
   
-  });
+//   });
   
